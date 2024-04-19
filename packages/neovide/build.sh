7187a00c64bbefea817558e02c27bd8c6ac35d5a
Jia Yuan Lo <jylo06g@gmail.com>
2024-04-19 08:25:37 +0800

    f

--- packages/neovide/build.sh
+++ packages/neovide/build.sh
@@ -20,11 +20,9 @@ termux_step_pre_configure() {
 	local f
 	for f in $CARGO_HOME/registry/src/*/winit-*/Cargo.toml; do
 		echo "Patching ${f}"
-		diff -u "${f}" <(sed 's/target_os = \\"android\\"/not(target_os = \\"android\\")/g' "${f}")
+		diff -u "${f}" <(sed 's/target_os = \\"android\\"/not(target_os = \\"android\\")/g' "${f}") --color
 		sed 's/target_os = \\"android\\"/not(target_os = \\"android\\")/g' -i "${f}"
 	done
-
-	grep android -nHR $CARGO_HOME/registry/src/*/*/Cargo.toml
 
 	#CFLAGS="$CPPFLAGS"
 }
